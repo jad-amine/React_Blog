5dc7c016ea26d54d5311f12ea5ccbe7a4d965b69
jad amine <jade1992amine@gmail.com>
2022-06-14 22:36:30 +0300

    First React App

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id:1},
+        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id:2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id:3}
+    ])
+    // const [name, setName] = useState('mario');
+    // const handleClickAgain = (name, e) => {     functions containing parameters should be invoked in anonymous functions so they don't run automatically
+    //     console.log('hello '+ name, e.target);   
+    // }
+
+    return ( 
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!" />
+        </div>
+     );
+}
+ 
+export default Home;
