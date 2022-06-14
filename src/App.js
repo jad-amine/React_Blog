1891775674d483e144d7a3c2ca48d237efbd1945
jad amine <jade1992amine@gmail.com>
2022-06-14 22:41:42 +0300

    Cleaned the code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Navbar from'./Navbar.js';
-import Home from './Home';
+import Navbar from "./Navbar.js";
+import Home from "./Home";
 
 function App() {
-  const title = 'Welcome to the new blog';
+  const title = "Welcome to the new blog";
   const likes = 50;
-  const array = ['jad', 'john', 50]
+  const array = ["jad", "john", 50];
   var link = "https://www.google.com";
 
   return (
     <div className="App">
-      <Navbar />                   {/* we want it to appear above the content */}
+      <Navbar /> {/* we want it to appear above the content */}
       <div className="content">
         <Home />
       </div>
