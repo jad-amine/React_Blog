7ef0bbf177d25582b12b99aa022986feb4c96190
jad amine <jade1992amine@gmail.com>
2022-06-16 06:53:52 +0300

    Added Delete Blog Functionality

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  // Allow to navigate browsing history and redirect
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +21,7 @@ const Create = () => {
       body: JSON.stringify(blog),
     }).then((res) => {
       setIsPending(false);
+      history.push("/");
     });
   };
 
